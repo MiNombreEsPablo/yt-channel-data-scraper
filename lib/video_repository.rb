7f175a82ce41eaf3a1470fe46425735d19827208
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-10-15 15:36:20 +0900

    defined generic repository methods

---
+++ lib/video_repository.rb
@@ -0,0 +1,31 @@
+require_relative 'video'
+# require_relative 'parsing_service'
+# require_relative 'crawling_service'
+require 'csv'
+require 'date'
+
+class VideoRepository
+  def initialize
+    @videos = []
+    # @parser = ParsingService.new
+    # @crawler = CrawlingService.new
+    @results = []
+  end
+
+  def all
+    @videos
+  end
+
+  def add(url)
+    @videos << Video.new(@parser.parse(url))
+  end
+
+  def crawl_list
+    @crawler.crawl
+  end
+
+  # the following method should be called with the user inputted url from the interface
+  def start()
+
+  end
+end
