20d8e5e96b91bce886eb9a35ffedaf4dd45fe723
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-10-15 14:12:15 +0900

    added pseudo code to interface

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,2 +1,7 @@
 # frozen_string_literal: true
 # TODO
+# get user input (a url)
+# start crawling service for the url
+# crawling service returns a list of video urls
+# create a video object for each url using crawling service
+# dump video repo to a csv file
