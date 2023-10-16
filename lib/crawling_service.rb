23dda9c337b073b5fcd50a72068af010bff4ca2e
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-10-16 21:23:11 +0900

    beta version

---
+++ lib/crawling_service.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+gem 'selenium-webdriver', '4.10.0'
+gem 'webdrivers', '5.3.1'
+
+require 'nokogiri'
+require 'watir'
+require 'webdrivers'
+
+class CrawlingService
+  attr_reader :list
+
+  def initialize
+    @previous_videos = 0
+    @list = []
+  end
+
+  def crawl(url)
+    browser = Watir::Browser.new :chrome, options: { args: %w[--remote-debugging-port=9222] }
+    browser.goto(url)
+
+    begin
+      goto_videos(browser)
+      sleep(1)
+      expand_loop(browser)
+      puts 'All videos loaded'
+      sleep(1)
+      html = browser.html
+      parse(html)
+       return @list
+    rescue StandardError => e
+      puts "An error occurred: #{e.message}"
+    ensure
+      browser.close
+    end
+  end
+
+  private
+
+  def goto_videos(browser)
+    puts 'Going to videos tab'
+    videos_tab = browser.element(text: 'Videos')
+    videos_tab.click
+  end
+
+  def expand_loop(browser)
+    loop do
+      current_videos = browser.elements(css: 'a#video-title-link').count
+
+      break if current_videos == @previous_videos
+
+      browser.scroll.by(0, 9_999_999_999)
+      sleep(1)
+      puts "Number of videos: #{current_videos}"
+      @previous_videos = current_videos
+    end
+  end
+
+  def parse(html)
+    doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+    links = doc.css('a#video-title-link')
+    links.each do |link|
+      @list << "https://www.youtube.com/#{link['href'].split('&')[0]}"
+    end
+  end
+end
--- lib/video_repository.rb
+++ lib/video_repository.rb
@@ -2,7 +2,7 @@
 
 require_relative 'video'
 require_relative 'parsing_service'
-# require_relative 'crawling_service'
+require_relative 'crawling_service'
 require 'csv'
 require 'date'
 
@@ -10,8 +10,9 @@ class VideoRepository
   def initialize
     @videos = []
     @parser = ParsingService.new
-    # @crawler = CrawlingService.new
+    @crawler = CrawlingService.new
     @results = []
+    @channel_url = ''
   end
 
   def all
@@ -19,13 +20,35 @@ class VideoRepository
   end
 
   def add(url)
+    video = @parser.parse(url)
+    video.merge!(channel_url: @channel_url)
     @videos << Video.new(@parser.parse(url))
   end
 
   def crawl_list
-    @crawler.crawl
+    @crawler.crawl(@channel_url)
   end
 
   # the following method should be called with the user inputted url from the interface
-  def start; end
+  def start
+    puts 'Enter a channel url'
+    @channel_url = gets.chomp
+    @results = crawl_list
+    @results.each do |url|
+      add(url)
+    end
+    dump_to_csv
+  end
+
+  private
+
+  def dump_to_csv
+    CSV.open('videos.csv', 'w') do |csv|
+      csv << %w[title url channel_url publication_date description duration view_count like_count sample_date]
+      @videos.each do |video|
+        csv << [video.title, video.url, video.channel_url, video.publication_date, video.description, video.duration,
+                video.view_count, video.like_count, Date.today]
+      end
+    end
+  end
 end
