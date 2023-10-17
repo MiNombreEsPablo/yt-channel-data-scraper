e11163bbe8a7840810858457eac1a8e48cef88fb
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-10-17 09:39:19 +0900

    subtitle test

--- lib/parsing_service.rb
+++ lib/parsing_service.rb
@@ -14,13 +14,16 @@ class ParsingService
       video_response = youtube.list_videos('snippet,statistics,contentDetails', id: video_id)
       video = video_response.items.first
 
+      subtitles = fetch_subtitles(youtube, video_id)
+
       result = {
         title: video.snippet.title,
         description: video.snippet.description,
         publication_date: video.snippet.published_at,
         duration: video.content_details.duration,
         view_count: video.statistics.view_count.to_i,
-        like_count: video.statistics.like_count.to_i
+        like_count: video.statistics.like_count.to_i,
+        subtitles: subtitles,
       }
 
       result.merge(url: url)
@@ -33,5 +36,12 @@ class ParsingService
 
   def extract_video_id(url)
     url.match(%r{(?:youtu\.be/|youtube\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/\s]{11})})[1]
+  end
+
+  def fetch_subtitles(youtube, video_id)
+    captions_response = youtube.list_captions('snippet', video_id: video_id)
+    captions = captions_response.items.map { |caption| caption.snippet.title }
+
+    captions.empty? ? 'No subtitles available' : captions.join(', ')
   end
 end
